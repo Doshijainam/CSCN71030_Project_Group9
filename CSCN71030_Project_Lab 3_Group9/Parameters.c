db0321fc7fc57baede18095da804de20a1060265
Cole MacGregor <colemacgregor31@gmail.com>
2022-03-17 12:55:11 -0400

    Added velocity functions for each planet. Velocity = distance/time. Having an issue with the time for sun and mars they're both returning 0.

--- CSCN71030_Project_Lab 3_Group9/Parameters.c
+++ CSCN71030_Project_Lab 3_Group9/Parameters.c
@@ -20,17 +20,14 @@ double distance(int destination) {
 		//Moon
 	case 1:
 		distance = 384400;
-		printf("\n You have chosen the moon!\n\n Its just a short trip of ");
 		break;
 		//Sun
 	case 2:
 		distance = 148750000;
-		printf("\n You have chosen the Sun!\n\n It's fairly warm there.\n It's ");
 		break;
 		//Mars
 	case 3:
 		distance = 286820000;
-		printf("\n You have chosen Mars!\n\n I hope you find some alien life.\n It's ");
 		break;
 	default:
 		EXIT_FAILURE;
@@ -54,23 +51,32 @@ void planetSelector() {
 		//If user inputted string is Moon then give the distance to the Moon
 		if (strcmp(planet, "Moon") == 0) {
 			choice = 1;
+			printf("\n You have chosen the moon!\n\n Its just a short trip of ");
 			printf("%.2lf", distance(choice));
 			printf(" kilometers!\n\n");
+			printf(" It will take you approx %d hours.\n\n", moonTime());
 			moonThrust();
+			moonVelocity();
 		}
 		//If user inputted string is Sun then give the distance to the Sun
 		else if (strcmp(planet, "Sun") == 0) {
 			choice = 2;
+			printf("\n You have chosen the Sun!\n\n It's fairly warm there.\n It's ");
 			printf("%.2lf", distance(choice));
 			printf(" kilometers from earth!\n You're gonna be here a while...\n\n");
+			printf(" It will take you approx %d hours.\n\n", sunThrust());
 			sunThrust();
+			sunVelocity();
 		}
 		//If user inputted string is Mars then give the distance to the Mars
 		else if (strcmp(planet, "Mars") == 0) {
 			choice = 3;
+			printf("\n You have chosen Mars!\n\n I hope you find some alien life.\n It's ");
 			printf("%.2lf", distance(choice));
 			printf(" kilometers from earth!\n You'll spend the rest of your life waiting...\n\n");
+			printf(" It will take you approx %d hours.\n\n", marsThrust());
 			marsThrust();
+			marsVelocity();
 		}
 		//If input is incorrect go back to entering a planet
 		else {
@@ -85,7 +91,6 @@ int moonTime() {
 	long int irand;
 	srand(time(NULL));
 	irand = rand() % (230 - 60 + 1) + 60;  //  for moon 
-	printf(" It will take you approx %d hours.\n\n", irand);
 	return irand;
 }
 //Sun time function
@@ -93,36 +98,52 @@ int sunTime() {
 	long int srand;
 	srand = rand() % (610 - 400 + 1) + 400;
 	// for sun it would take 606 hours 
-	printf(" It will take you approx %d hours.\n\n", srand);
 	return srand;
 }
 //Mars time function
 int marsTime() {
 	long int Rrand;
 	Rrand = rand() % (5100 - 5000 + 1) + 5000;
-	printf(" It will take you approx %d hours.\n\n", Rrand);
 	return Rrand;
-}
-
-double velocity() {
-	return 0;
 }
 
 //Thrust for moon = mass / time
 double moonThrust() {
 	double thrust = MASS / moonTime();
-	printf(" Rocket thrust will be %.2lf.\n\n", thrust);
+	printf(" Rocket thrust will be %.2lf Newtons\n\n", thrust);
 	return thrust;
 }
 //Thrust for sun = mass / time
 double sunThrust() {
 	double thrust = MASS / sunTime();
-	printf(" Rocket thrust will be %.2lf.\n\n", thrust);
+	printf(" Rocket thrust will be %.2lf Newtons\n\n", thrust);
 	return thrust;
 }
 //Thrust for mars = mass / time
 double marsThrust() {
 	double thrust = MASS / marsTime();
-	printf(" Rocket thrust will be %.2lf.\n\n", thrust);
+	printf(" Rocket thrust will be %.2lf Newtons\n\n", thrust);
 	return thrust;
+}
+
+//Velocity for moon = distance / time
+double moonVelocity()
+{
+	double velocity = distance(1) / moonTime();
+	printf(" The Velocity of the rocket will be %.2lf Km/h.\n\n", velocity);
+	return velocity;
+}
+//Velocity for sun = distance / time
+double sunVelocity()
+{
+	double velocity = distance(2) / sunTime();
+	printf(" The Velocity of the rocket will be %.2lf Km/h.\n\n", velocity);
+	return velocity;
+}
+//Velocity for mars = distance / time
+double marsVelocity()
+{
+	double velocity = distance(3) / marsTime();
+	printf(" The Velocity of the rocket will be %.2lf Km/h.\n\n", velocity);
+	return velocity;
 }
