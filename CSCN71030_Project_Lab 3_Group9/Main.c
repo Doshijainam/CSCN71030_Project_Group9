af8b4c6245f8526b3e206f09e4973c8a0d7a7a04
Cole MacGregor <colemacgregor31@gmail.com>
2022-03-25 16:38:04 -0400

    Fixed main so file handling works.

--- CSCN71030_Project_Lab 3_Group9/Main.c
+++ CSCN71030_Project_Lab 3_Group9/Main.c
@@ -11,18 +11,30 @@
 #include<stdlib.h>
 #include <string.h>
 #include "Parameter.h"
+#include "ParameterStructure.h"
+#include "Files.h"
 
 #define MAX_STRING 100
 
 int main(int argc, char *argv[])
 {
+	//Check if a file exists, if not make a new file
+	if (checkIfFileExists("file.txt")) {
+		readOpenFile();
+	}
+	else {
+		writeOpenFile();
+	}
+
+	//Allocate memory for the structure
+	struct parameters* save = (struct parameters*)malloc(sizeof(struct parameters));
 
 	//Function to have rocket launch animation at start
 	rocketLaunch();
 
 	//Prints intro menu
 	menu();
-	
+
 	//Calls function for user to select a planet
 	planetSelector();
 }
