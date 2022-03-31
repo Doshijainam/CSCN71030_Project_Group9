8d9631539743669b743c47823954171b32a2c2fa
Cole MacGregor <colemacgregor31@gmail.com>
2022-03-31 11:19:54 -0400

    Added some cosmetic changes.

--- CSCN71030_Project_Lab 3_Group9/characteristics.c
+++ CSCN71030_Project_Lab 3_Group9/characteristics.c
@@ -12,9 +12,6 @@
 
 #define MAX 1000 
 
-	
-
-
 struct Characteristics {
 	 
 	int mass; 
@@ -24,8 +21,8 @@ struct Characteristics {
 	char TypeofRocket[MAX]; 
 	double  Temperature[MAX]; 
 
-	
 }characteristics;
+
 struct Characteristics characteristics;
 
  int  CalculatingMass() {
@@ -33,68 +30,42 @@ struct Characteristics characteristics;
 	srand(time(NULL)); 
 
 	characteristics.mass = rand() % (600 - 500 + 1) + 500; 
-	// Maximum mass of spacecraft  is 600 and minimum is 500  
 	
-	 
-
+	// Maximum mass of spacecraft  is 600 and minimum is 500  
 	return characteristics.mass; 
-
-
 }
 
  char * TypesofRocket() {
 
 	
 
-	 printf("Enter the type of rocket you want \n It can be in  either Solid or in liquid state \n"); 
-	 char Type[MAX] = "\0";
+	 printf("Enter the type of rocket you want \nIt can be in  either Solid or in liquid state: ");
 
+		 char Type[MAX] = "\0";
 
 		 scanf_s("%s", Type,MAX); 
 		 strcpy(characteristics.TypeofRocket, Type); 
-	 
-
 	
-		 printf("The type of Rocket is %s \n",characteristics.TypeofRocket); 
+		 printf("The type of Rocket is %s. \n\n",characteristics.TypeofRocket); 
 
 	 
 	 return characteristics.TypeofRocket; 
-
-
-
  }
 
  char* DeterminingBodymaterials() {
 
-	 printf("Enter the material you want to use for the spacecraft  \n");
+	 printf("Enter the material you want to use for the spacecraft: ");
 	 char Material[MAX] = "\0";
 
 
 	 scanf_s("%s", Material, MAX);
 	 strcpy(characteristics.body, Material);
 
+	 printf("The material that we are going to use is %s. \n\n", characteristics.body);
 
 
-	 printf("The material that we are going to use  is %s \n", characteristics.body);
-
-
-	 return characteristics.body;
-
-
-
- 
-	
-		 
-
-
-	  
-	 
+	 return characteristics.body; 
  }
-
-	 
-
-
-
 
 double* CalculatingTemperatureMoon() {
 	const int Destination = 3; 
@@ -102,15 +73,11 @@ double* CalculatingTemperatureMoon() {
 
 	for (int i = 0; i < Destination; ++i) {
 		for (int j = 0; j <3; ++j) {
-			printf("Destination %d , Day %d\n", i + 1, j + 1); 
-			printf("Enter the temperature \n"); 
+			printf("Destination %d , Day %d\n", i + 1, j + 1);
+			printf("Enter the temperature: ");
 			scanf_s("%lf", &characteristics.Temperature[i]); 
-			printf("Temperature at Destination %d is %lf", i +1, characteristics.Temperature[i]); 
-
-
+			printf("Temperature at Destination %d is %.2lf\n", i + 1, characteristics.Temperature[i]);
 		}
 	}
 	return characteristics.Temperature;
-
-
 }
